2221762f0d820046b86fabb9af4f7197e6385fa0
CanhTruongTrong <trongcanhtruong@gmail.com>
2023-06-18 16:38:26 +0700

    update code

--- B9_Class/main.cpp
+++ B9_Class/main.cpp
@@ -1,30 +1,38 @@
 #include <iostream>
+#include <string>
 
 using namespace std;
 
-class SinhVien{
-    public:
-    void hienThi();
-    void themThongTin(int tuoi, int lop); // method
-    
+class DoiTuong{
+    public: // phạm vi truy cập
+        void setThongTin(string ten, int tuoi); // method
+        void hienThi();
     private:
-    int tuoi; // property
-    int lop;
+        string TEN; // property
+        int TUOI;
 };
 
-void SinhVien::hienThi(){
-    cout<<"Tuoi: "<<tuoi<<endl;
-    cout<<"Lop: "<<lop<<endl;
+void DoiTuong::setThongTin(string ten, int tuoi){
+    TEN = ten;
+    TUOI = tuoi;
 }
 
-void SinhVien::themThongTin(int tuoi, int lop){
-    SinhVien::tuoi = tuoi;
-    SinhVien::lop = lop;
+void DoiTuong::hienThi(){
+    cout<<"Day la class DoiTuong\n";
+    cout<<"Ten: "<<TEN<<endl;
+    cout<<"Tuoi: "<<TUOI<<endl;
 }
+
+class SinhVien: public DoiTuong{        // SinhVien kế thừa public của DoiTuong
+
+};
 
 int main(){
-    SinhVien sv; // object sv thuộc class SinhVien
+    DoiTuong dt; // object
+    dt.setThongTin("Canh", 23);
+    dt.hienThi();
 
-    sv.themThongTin(16,10);
+    SinhVien sv;
+    sv.setThongTin("Hieu", 15);
     sv.hienThi();
 }
